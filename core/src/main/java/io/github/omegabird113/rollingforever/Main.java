fdf895a5f80ea3720bc6c1f3884bfcc117ddb48d
omegabird113 <omegabird113@outlook.com>
2026-05-24 10:27:30 -0400

    Initial commit to local git repo

---
+++ core/src/main/java/io/github/omegabird113/rollingforever/Main.java
@@ -0,0 +1,70 @@
+package io.github.omegabird113.rollingforever;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private PerspectiveCamera camera;
+    private Environment environment;
+    private Array<ModelInstance> instances;
+    private ModelBatch modelBatch;
+    private final Color BACKGROUND_COLOUR = new Color(153f/255f, 255f/255f, 236f/255f, 1.0f);
+    private final Room room = new Room();
+
+    @Override
+    public void create() {
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(),  Gdx.graphics.getHeight());
+        camera.position.set(0f, 95f, -17f);
+        camera.lookAt(0,0,0);
+        camera.near = 0.1f;
+        camera.far = 30000f;
+        camera.update();
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.6f, 0.6f, 0.6f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+
+        // create and position model instances
+        instances = new Array<>();
+        instances.add(room.createGround(modelBuilder));
+
+        modelBatch = new ModelBatch();
+    }
+
+    @Override
+    public void render() {
+        // render
+        ScreenUtils.clear(BACKGROUND_COLOUR);
+        modelBatch.begin(camera);
+        modelBatch.render(instances, environment);
+        modelBatch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
+    }
+
+    @Override
+    public void dispose() {
+        modelBatch.dispose();
+        room.disposeGround();
+    }
+}
---
+++ core/src/main/java/io/github/omegabird113/rollingforever/Room.java
@@ -0,0 +1,37 @@
+package io.github.omegabird113.rollingforever;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+
+public class Room {
+    private Model modelGround;
+    private Texture textureGround;
+
+    public ModelInstance createGround(ModelBuilder modelBuilder) {
+        textureGround = new Texture(Gdx.files.internal("libgdx.png"), true);
+        textureGround.setFilter(Texture.TextureFilter.MipMapLinearLinear, Texture.TextureFilter.Linear);
+        textureGround.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+        TextureRegion textureRegion = new TextureRegion(textureGround);
+        int repeats = 10;
+        textureRegion.setRegion(0,0,textureGround.getWidth()*repeats, textureGround.getHeight()*repeats );
+
+        // create model
+        modelGround = modelBuilder.createBox(100f, 1f, 100f,
+            new Material(TextureAttribute.createDiffuse(textureRegion)),
+            VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
+
+        return new ModelInstance(modelGround, 0, -1, 0);
+    }
+
+    public void disposeGround() {
+        modelGround.dispose();
+        textureGround.dispose();
+    }
+}
