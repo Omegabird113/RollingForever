c8b10e18fb37bc646f5a837afd7e1c2bc5a22ed7
Omegabird113 <137644096+Omegabird113@users.noreply.github.com>
2026-05-28 18:18:46 -0400

    Temporarily disable WebAssembly for TeaVM

--- teavm/src/main/java/io/github/omegabird113/rollingforever/teavm/TeaVMBuilder.java
+++ teavm/src/main/java/io/github/omegabird113/rollingforever/teavm/TeaVMBuilder.java
@@ -24,7 +24,7 @@ public class TeaVMBuilder {
                 .setHtmlWidth(1280)
                 .setHtmlHeight(720)
                 .setHtmlTitle("Rolling Forever")
-                .setWebAssembly(true)
+                .setWebAssembly(false)
                 .setStartJettyAfterBuild(startJetty)
                 .setJettyPort(8080)
         )
