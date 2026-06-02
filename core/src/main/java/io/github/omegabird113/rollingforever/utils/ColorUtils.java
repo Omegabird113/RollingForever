76916280d7ef8bf1b2a8a7008ef226a9fbe54588
omegabird113 <omegabird113@outlook.com>
2026-06-02 18:51:34 -0400

    Some ColorUtils features because I can

--- core/src/main/java/io/github/omegabird113/rollingforever/utils/ColorUtils.java
+++ core/src/main/java/io/github/omegabird113/rollingforever/utils/ColorUtils.java
@@ -1,10 +1,17 @@
 package io.github.omegabird113.rollingforever.utils;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public class ColorUtils {
     public static final Color BLACK = get8BitColor(0, 0, 0);
     public static final Color WHITE = get8BitColor(255, 255, 255);
+    public static final Color PURE_RED = get8BitColor(255, 0, 0);
+    public static final Color PURE_GREEN = get8BitColor(255, 0, 255);
+    public static final Color PURE_BLUE = get8BitColor(0, 0, 255);
 
     public static Color get8BitColor(int r, int g, int b, int a) {
         return new Color(r / 255f, g / 255f, b / 255f, a / 255f);
@@ -12,5 +19,35 @@ public class ColorUtils {
 
     public static Color get8BitColor(int r, int g, int b) {
         return get8BitColor(r, g, b, 255);
+    }
+
+    public static Color getNBitColor(int n, int r, int g, int b, int a) {
+        return new Color(r / (float) n, g / (float) n, b / (float) n, a / (float) n);
+    }
+
+    public static Color getNBitColor(int n, int r, int g, int b) {
+        return getNBitColor(n, r, g, b, 255);
+    }
+
+    public static Color getAverageColor(Collection<Color> colors) {
+        Array<Float> rValues = new Array<>();
+        Array<Float> gValues = new Array<>();
+        Array<Float> bValues = new Array<>();
+
+        for (Color color : colors) {
+            rValues.add(color.r);
+            gValues.add(color.g);
+            bValues.add(color.b);
+        }
+
+        float rSum = Arrays.stream(rValues.toArray()).count();
+        float gSum = Arrays.stream(gValues.toArray()).count();
+        float bSum = Arrays.stream(bValues.toArray()).count();
+
+        float rAverage = rSum / rValues.size;
+        float gAverage = gSum / gValues.size;
+        float bAverage = bSum / bValues.size;
+
+        return new Color(rAverage, gAverage, bAverage, 1f);
     }
 }
